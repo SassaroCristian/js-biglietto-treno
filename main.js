b1ffb1060cdfc67e207ef202d944b3edd373825c
Zazzaro <sassaro01cristian@gmail.com>
2023-10-27 10:38:54 +0200

    more fixes

--- main.js
+++ main.js
@@ -11,11 +11,12 @@ const totalPrice = document.getElementById('prezzo');
 const salePrice = document.getElementById('prezzoScontato');
 
 if (age < 18) {
-    totalPrice.textContent = `Spenderai ${priceKilometer} (senza sconto)`;
-    salePrice.textContent = `Spenderai ${(priceKilometer - under18sale).toFixed(2)} (con sconto del 20%)`;
+    totalPrice.textContent = `Prezzo intero ${priceKilometer}`;
+    salePrice.textContent = `Prezzo scontato del 20% ${(priceKilometer - under18sale).toFixed(2)}`;
 } else if (age >= 18 && age < 65) {
     totalPrice.textContent = `Spenderai ${priceKilometer}`;
 } else if (age >= 65) {
-    totalPrice.textContent = `Spenderai ${priceKilometer} (senza sconto)`;
-    salePrice.textContent = `Spenderai ${(priceKilometer - over65sale).toFixed(2)} (con sconto del 40%)`;
+    totalPrice.textContent = `Prezzo intero ${priceKilometer}`;
+    salePrice.textContent = `Prezzo scontato del 40% ${(priceKilometer - over65sale).toFixed(2)}`;
 }
+
