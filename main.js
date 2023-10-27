498ef553796f092b9944eeee16a6b518eb7f5336
Zazzaro <sassaro01cristian@gmail.com>
2023-10-27 11:01:28 +0200

    added error message

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 let distance = parseFloat(prompt('Inserisci la distanza che devi percorrere'));
 let age = parseInt(prompt('Inserisci la tua età'));
 
+// CALCOLO COSTO 
 const priceOneKilometer = 0.21;
 let priceKilometer = distance * priceOneKilometer;
 
+// CALCOLO SCONTISTICA
 const under18sale = priceKilometer * 0.20; 
 const over65sale = priceKilometer * 0.40; 
+// 
 
 const totalPrice = document.getElementById('prezzo');
 const salePrice = document.getElementById('prezzoScontato');
 
+// 
 if (age < 18) {
     totalPrice.textContent = `Prezzo intero ${priceKilometer}`;
     salePrice.textContent = `Prezzo scontato del 20% ${(priceKilometer - under18sale).toFixed(2)}`;
@@ -18,5 +22,7 @@ if (age < 18) {
 } else if (age >= 65) {
     totalPrice.textContent = `Prezzo intero ${priceKilometer}`;
     salePrice.textContent = `Prezzo scontato del 40% ${(priceKilometer - over65sale).toFixed(2)}`;
+} else {
+    totalPrice.textContent = `Errore`
 }
 
